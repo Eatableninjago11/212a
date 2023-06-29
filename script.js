953ac4df60cecd1ffc241cf68f03581a34e8f399
Eatableninjago11 <83182611+Eatableninjago11@users.noreply.github.com>
2023-06-29 13:54:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+$(function(){
+    $('#show_chat').click(function(){
+        console.log("chat button clicked");
+        $(".left-window").css("display", "none")
+        $(".right-window").css("display", "block")
+        $(".header_back").css("display", "block")
+    })
+
+    $('.header_back').click(function(){
+        $(".left-window").css("display", "block")
+        $(".right-window").css("display", "none")
+        $(".header_back").css("display", "none")
+    })
+})
+
